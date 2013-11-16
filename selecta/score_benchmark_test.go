5c71c78f2b2090548450a1a0ae98147bcda1ac42
Thomas Boyt <thomas.boyt@venmo.com>
2013-11-15 22:35:46 -0500

    Fill out benchmark

--- selecta/score_benchmark_test.go
+++ selecta/score_benchmark_test.go
@@ -7,7 +7,7 @@ import (
 	"testing"
 )
 
-func getChoices() []string {
+func makeChoices() []string {
 	f, _ := os.Open("words.txt")
 
 	bf := bufio.NewReader(f)
@@ -25,23 +25,72 @@ func getChoices() []string {
 	return choices
 }
 
-func BenchmarkUnmatchedScore(b *testing.B) {
-	choices := getChoices()
+func generateChoices(chars string, reps int, n int) []string {
+	strs := make([]string, n)
+	for i := 0; i < n; i++ {
+		str := ""
+		for j := 0; j < reps; j++ {
+			str += chars
+		}
+		strs[i] = str
+	}
+	return strs
+}
 
-	b.ResetTimer()
+func BenchmarkNonMatching(b *testing.B) {
+	choices := generateChoices("x", 16, 1000)
+	query := "yyyyyyyyyyyyyyyy"
 
 	for i := 0; i < b.N; i++ {
 		for _, choice := range choices {
-			Score(choice, "xxxxxxxxxxxxxxxx")
+			Score(choice, query)
+		}
+	}
+}
+
+var query string = "xxxxxxxxxxxxxxxx"
+
+func BenchmarkMatchingExactly(b *testing.B) {
+	choices := generateChoices("x", 16, 1000)
+
+	for i := 0; i < b.N; i++ {
+		for _, choice := range choices {
+			Score(choice, query)
+		}
+	}
+}
+
+func BenchmarkMatchingBrokenUp(b *testing.B) {
+	choices := generateChoices("xy", 16, 1000)
+
+	for i := 0; i < b.N; i++ {
+		for _, choice := range choices {
+			Score(choice, query)
+		}
+	}
+}
+
+func BenchmarkOverlappingMatches(b *testing.B) {
+	choices := generateChoices("x", 40, 1000)
+
+	for i := 0; i < b.N; i++ {
+		for _, choice := range choices {
+			Score(choice, query)
+		}
+	}
+}
+
+var choices []string = makeChoices()
+
+func BenchmarkUnmatchedScore(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, choice := range choices {
+			Score(choice, query)
 		}
 	}
 }
 
 func BenchmarkMatchedScore(b *testing.B) {
-	choices := getChoices()
-
-	b.ResetTimer()
-
 	for i := 0; i < b.N; i++ {
 		for _, choice := range choices {
 			Score(choice, "ungovernableness")
